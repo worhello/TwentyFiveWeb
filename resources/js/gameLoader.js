325cfd3d9c5af3e209a098b009671141f20c29f7
worhello <worhello@users.noreply.github.com>
2020-12-21 16:16:12 +0000

    Add centralised localisation part 1 (#104)

--- resources/js/gameLoader.js
+++ resources/js/gameLoader.js
@@ -44,7 +44,7 @@ function onStartButtonClicked() {
 }
 
 function onTutorialButtonClicked() {
-    let userIsOk = window.confirm("Running a game in Tutorial mode results in having predefined card choices, to highlight the different card interactions and comparisons. You will generally have far more card choices available in a real game.\n\nClick OK to continue");
+    let userIsOk = window.confirm(window.localisationManager.getLocalisedString("tutorialConfirmationMessage"));
     if (userIsOk === false) {
         return;
     }
@@ -63,6 +63,35 @@ function preloadCards() {
     }
 }
 
+function initLocalisation() {
+    let locale = "en/UK"; // TODO
+    window.localisationManager = new window.localisedStrings.LocalisedStringManager(locale);
+
+    document.getElementById("2PlayersOption").textContent  = window.localisationManager.getLocalisedString("2PlayersOption");
+    document.getElementById("3PlayersOption").textContent  = window.localisationManager.getLocalisedString("3PlayersOption");
+    document.getElementById("4PlayersOption").textContent  = window.localisationManager.getLocalisedString("4PlayersOption");
+    document.getElementById("5PlayersOption").textContent  = window.localisationManager.getLocalisedString("5PlayersOption");
+    document.getElementById("6PlayersOption").textContent  = window.localisationManager.getLocalisedString("6PlayersOption");
+    document.getElementById("7PlayersOption").textContent  = window.localisationManager.getLocalisedString("7PlayersOption");
+    document.getElementById("8PlayersOption").textContent  = window.localisationManager.getLocalisedString("8PlayersOption");
+    document.getElementById("9PlayersOption").textContent  = window.localisationManager.getLocalisedString("9PlayersOption");
+    document.getElementById("10PlayersOption").textContent = window.localisationManager.getLocalisedString("10PlayersOption");
+
+    document.getElementById("singlePlayerLabel").textContent = window.localisationManager.getLocalisedString("singlePlayer");
+    document.getElementById("multiPlayerLabel").textContent  = window.localisationManager.getLocalisedString("multiPlayerComingSoon");
+    document.getElementById("slowSpeedLabel").textContent    = window.localisationManager.getLocalisedString("slowSpeedLabel");
+    document.getElementById("mediumSpeedLabel").textContent  = window.localisationManager.getLocalisedString("mediumSpeedLabel");
+    document.getElementById("fastSpeedLabel").textContent    = window.localisationManager.getLocalisedString("fastSpeedLabel");
+    
+    document.getElementById("startGameButton").textContent     = window.localisationManager.getLocalisedString("startGameButton");
+    document.getElementById("startTutorialButton").textContent = window.localisationManager.getLocalisedString("startTutorialButton");
+
+    document.getElementById("gameRulesLabel").textContent    = window.localisationManager.getLocalisedString("gameRulesLabel");
+
+    document.getElementById("currentCardIndicator").textContent        = window.localisationManager.getLocalisedString("currentCardIndicator");
+    document.getElementById("currentWinningCardIndicator").textContent = window.localisationManager.getLocalisedString("currentWinningCardIndicator");
+}
+
 window.onload = function() {
     this.document.getElementById("startGameButton").addEventListener("click", function() {
         onStartButtonClicked();
@@ -71,6 +100,7 @@ window.onload = function() {
         onTutorialButtonClicked();
     });
 
+    initLocalisation();
     showStartGameOverlay();
     preloadCards();
 }
