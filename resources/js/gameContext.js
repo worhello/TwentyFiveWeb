391852a56a76346ff225b6a598961244038b351c
unknown <worhello@gmail.com>
2021-11-18 20:45:36 +0000

    Unpack json args in handleTfEvent method

--- resources/js/gameContext.js
+++ resources/js/gameContext.js
@@ -14,16 +14,16 @@ class GameContext {
     }
 
     
-    async handlePlayerListChanged(json) {
+    async handlePlayerListChanged(playersDetails, needMorePlayers) {
         // no-op for SinglePlayer, very useful for MultiPlayer
     }
 
-    async handleGameInitialState(json) {
-        this.trumpCard = json.gameInfo.trumpCard;
-        this.selfPlayer.id = json.playerDetails.userId;
-        this.selfPlayer.cards = json.playerDetails.cards;
-        this.players = json.players;
-        this.teams = json.teams;
+    async handleGameInitialState(gameInfo, playerDetails, players, teams) {
+        this.trumpCard = gameInfo.trumpCard;
+        this.selfPlayer.id = playerDetails.userId;
+        this.selfPlayer.cards = playerDetails.cards;
+        this.players = players;
+        this.teams = teams;
         this.setSelfPlayer(this.players);
         await this.notifyGameInitialState(this.selfPlayer, this.players, this.trumpCard, this.teams);
     }
@@ -38,54 +38,53 @@ class GameContext {
         }
     }
 
-    async handleCurrentPlayerMovePending(json) {
-        let player = this.players.find(function (p) { return p.id == json.userId; });
+    async handleCurrentPlayerMovePending(userId) {
+        let player = this.players.find(function (p) { return p.id == userId; });
         await this.eventsHandler.sendEventToViewController('highlightCurrentPlayer', { "player": player });
     }
 
-    async handlePlayerMoveRequested(json) {
-        if (json.userId == this.selfPlayer.id) {
+    async handlePlayerMoveRequested(userId) {
+        if (userId == this.selfPlayer.id) {
             await this.eventsHandler.sendEventToViewController('showSelfPlayerHand', { "selfPlayer": this.selfPlayer, "isEnabled": true });
         }
     }
 
-    async handleCardPlayed(json) {
-        let player = this.players.find(function (p) { return p.id == json.userId; });
-        let playedCard = json.playedCard;
+    async handleCardPlayed(userId, playedCard, isNewWinningCard) {
+        let player = this.players.find(function (p) { return p.id == userId; });
         await this.eventsHandler.sendEventToViewController('playCard', { "player": player, "playedCard": playedCard });
-        if (json.isNewWinningCard == true) {
+        if (isNewWinningCard == true) {
             await this.eventsHandler.sendEventToViewController('updateCurrentWinningCard', { "player": player, "card": playedCard });
         }
     }
 
-    async handleCardsUpdated(json) {
-        if (json.userId == this.selfPlayer.id) {
-            this.selfPlayer.cards = json.cards;
+    async handleCardsUpdated(userId, cards) {
+        if (userId == this.selfPlayer.id) {
+            this.selfPlayer.cards = cards;
             await this.eventsHandler.sendEventToViewController('showSelfPlayerHand', { "selfPlayer": this.selfPlayer, "isEnabled": false });
         }
     }
 
-    async handleTeamGameEndOfHandOrGame(json, gameFinished) {
+    async handleTeamGameEndOfHandOrGame(teams, orderedPlayers, gameFinished) {
         let teamPlayersInfos = [];
-        for (let team of json.teams) {
+        for (let team of teams) {
             let teamPlayersInfo = {};
             teamPlayersInfo.teamId = team.id;
             teamPlayersInfo.totalScore = team.totalScore;
             teamPlayersInfo.players = [];
             for (let playerId of team.playerIds) {
-                teamPlayersInfo.players.push(json.orderedPlayers.find(p => p.id == playerId));
+                teamPlayersInfo.players.push(orderedPlayers.find(p => p.id == playerId));
             }
             teamPlayersInfos.push(teamPlayersInfo);
         }
         await this.eventsHandler.sendEventToViewController('showEndOfHandOrGameStats_teams', { "teamPlayersInfos": teamPlayersInfos, "gameFinished": gameFinished });
     }
 
-    async handleGameFinished(json) {
+    async handleGameFinished(teams, orderedPlayers) {
         if (this.teams.length == 0) {
-            await this.eventsHandler.sendEventToViewController('showGameEndScreen', { "sortedPlayers": json.orderedPlayers });
+            await this.eventsHandler.sendEventToViewController('showGameEndScreen', { "sortedPlayers": orderedPlayers });
         }
         else {
-            await this.handleTeamGameEndOfHandOrGame(json, true);
+            await this.handleTeamGameEndOfHandOrGame(teams, orderedPlayers, true);
         }
     }
 
@@ -93,23 +92,23 @@ class GameContext {
         players.find(p => p.id == this.selfPlayer.id).isSelfPlayer = true;
     }
 
-    async handleRoundFinished(json) {
-        this.setSelfPlayer(json.orderedPlayers);
-        var promises = [ this.handleScoresUpdated(json) ];
+    async handleRoundFinished(teams, orderedPlayers) {
+        this.setSelfPlayer(orderedPlayers);
+        var promises = [ this.handleScoresUpdated(orderedPlayers) ];
         if (this.teams.length == 0) {
-            promises.push(this.eventsHandler.sendEventToViewController('showEndOfHandStats', { "sortedPlayers": json.orderedPlayers }));
+            promises.push(this.eventsHandler.sendEventToViewController('showEndOfHandStats', { "sortedPlayers": orderedPlayers }));
         }
         else {
-            await this.handleTeamGameEndOfHandOrGame(json, false);
+            await this.handleTeamGameEndOfHandOrGame(teams, orderedPlayers, false);
         }
         for (let p of promises) {
             await p;
         }
     }
 
-    handleScoresUpdated(json) {
-        for (var i = 0; i < json.orderedPlayers.length; i++) {
-            let updatedPlayer = json.orderedPlayers[i];
+    handleScoresUpdated(orderedPlayers) {
+        for (var i = 0; i < orderedPlayers.length; i++) {
+            let updatedPlayer = orderedPlayers[i];
             let playerIndex = this.players.findIndex(function (p) { return p.id == updatedPlayer.id; });
             if (playerIndex > -1) {
                 this.players[playerIndex].score = updatedPlayer.score;
@@ -117,55 +116,55 @@ class GameContext {
         }
     }
 
-    async handleRobTrumpCardAvailable(json) {
+    async handleRobTrumpCardAvailable(trumpCard) {
         await this.eventsHandler.sendEventToViewController('showSelfPlayerRobbingDialog', { 
-            "trumpCard": json.trumpCard, 
+            "trumpCard": trumpCard, 
             "skipButtonDisabled": false,
             "skipButtonDisabledReason": ""
         });
     }
 
-    async handlePlayersReadyForNextRoundChanged(json) {
+    async handlePlayersReadyForNextRoundChanged(readyPlayerIds) {
         await this.eventsHandler.sendEventToViewController('playersReadyForNextRoundChanged', { 
-            "readyPlayers": this.players.filter(p => json.readyPlayerIds.findIndex(pId => pId == p.id) != -1),
-            "disableButtons": json.readyPlayerIds.indexOf(this.userId) != -1
+            "readyPlayers": this.players.filter(p => readyPlayerIds.findIndex(pId => pId == p.id) != -1),
+            "disableButtons": readyPlayerIds.indexOf(this.userId) != -1
         });
     }
 
     async handleTfGameEvent(json) {
         // console.log(json);
         if (json.type == "playerListChanged") {
-            await this.handlePlayerListChanged(json);
+            await this.handlePlayerListChanged(json.playersDetails, json.needMorePlayers);
         }
         else if (json.type == "gameInitialState") {
-            await this.handleGameInitialState(json);
+            await this.handleGameInitialState(json.gameInfo, json.playerDetails, json.players, json.teams);
         }
         else if (json.type == "currentPlayerMovePending") {
-            await this.handleCurrentPlayerMovePending(json);
+            await this.handleCurrentPlayerMovePending(json.userId);
         }
         else if (json.type == "playerMoveRequested") {
-            await this.handlePlayerMoveRequested(json);
+            await this.handlePlayerMoveRequested(json.userId);
         }
         else if (json.type == "cardPlayed") {
-            await this.handleCardPlayed(json);
+            await this.handleCardPlayed(json.userId, json.playedCard, json.isNewWinningCard);
         }
         else if (json.type == "cardsUpdated") {
-            await this.handleCardsUpdated(json);
+            await this.handleCardsUpdated(json.userId, json.cards);
         }
         else if (json.type == "gameFinished") {
-            await this.handleGameFinished(json);
+            await this.handleGameFinished(json.teams, json.orderedPlayers);
         }
         else if (json.type == "roundFinished") {
-            await this.handleRoundFinished(json);
+            await this.handleRoundFinished(json.teams, json.orderedPlayers);
         }
         else if (json.type == "scoresUpdated") {
-            this.handleScoresUpdated(json);
+            this.handleScoresUpdated(json.orderedPlayers);
         }
         else if (json.type == "robTrumpCardAvailable") {
-            await this.handleRobTrumpCardAvailable(json);
+            await this.handleRobTrumpCardAvailable(json.trumpCard);
         }
         else if (json.type == "playersReadyForNextRoundChanged") {
-            await this.handlePlayersReadyForNextRoundChanged(json);
+            await this.handlePlayersReadyForNextRoundChanged(json.readyPlayerIds);
         }
     }
 }
--- resources/js/multiPlayerGameContext.js
+++ resources/js/multiPlayerGameContext.js
@@ -163,10 +163,9 @@ class MultiPlayerGameContext extends GameContext {
         }
     }
 
-    handlePlayerListChanged(json) {
+    handlePlayerListChanged(playersDetails, needMorePlayers) {
         // Caution - if we end up changing players midway through we lose all details here
         let gameContext = this;
-        let playersDetails = json.playersDetails;
         let playersModule = getPlayerModule();
 
         // TODO - make better way to build up TF types from Json
@@ -181,10 +180,10 @@ class MultiPlayerGameContext extends GameContext {
 
         this.eventsHandler.sendEventToViewController('updateMultiplayerWaitingScreen', {
             waitingPlayers: this.players,
-            needMorePlayers: json.needMorePlayers,
+            needMorePlayers: needMorePlayers,
             gameUrl: this.gameUrl,
             buttonsEnabled: this.players.find(p => p.id == this.userId).isHost,
-            continueFunc: function () { json.needMorePlayers ? gameContext.requestAIs() : gameContext.startGameOnServer(); }
+            continueFunc: function () { needMorePlayers ? gameContext.requestAIs() : gameContext.startGameOnServer(); }
         });
 
         if (!this.selfPlayer) {
